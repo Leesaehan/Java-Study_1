80ee4d509582573a352facfa53473c177e388731
Leesaehan <77419064+Leesaehan@users.noreply.github.com>
2021-01-31 21:42:56 +0900

    3주차 과제

---
+++ For_Exam_01.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class For_Exam_01 {
+	
+public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int a = 0;
+		a = sc.nextInt();
+		
+		for (int i = 0; i < a; i++) {
+			System.out.print("*");
+		}
+		
+	}
+}
